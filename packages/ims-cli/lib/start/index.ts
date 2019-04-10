96b5dd8621ef9b0c262262da76430d9bd4b5455f
imeepos <1037483576@qq.com>
2019-04-10 15:02:36 +0800

    ims-cli:build

--- packages/ims-cli/lib/start/index.ts
+++ packages/ims-cli/lib/start/index.ts
@@ -2,6 +2,7 @@ import { Command, Input } from 'ims-core';
 import { ImsCommand } from '../command';
 import { join } from 'path';
 import { ImsCommandPm2 } from '../pm2'
+import { exec } from 'shelljs'
 @Command({
     name: 'start'
 })
@@ -13,18 +14,22 @@ export class ImsStart extends ImsCommand {
 
     async run() {
         const pm2 = new ImsCommandPm2();
-        pm2.script = join(__dirname, 'bin/build.js');
-        pm2.name = 'template';
-        pm2.run();
-        if (this.type === 'dev') {
-            pm2.script = join(__dirname, 'bin/dev.js');
-            pm2.name = 'dev';
-            await pm2.run();
-        }
-        if (this.type === 'prod') {
-            pm2.script = join(__dirname, 'bin/prod.js');
-            pm2.name = 'prod';
-            await pm2.run();
-        }
+        exec(`pm2 kill`, {
+            cwd: process.cwd()
+        }, async () => {
+            pm2.script = join(__dirname, 'bin/build.js');
+            pm2.name = 'template';
+            pm2.run();
+            if (this.type === 'dev') {
+                pm2.script = join(__dirname, 'bin/dev.js');
+                pm2.name = 'dev';
+                await pm2.run();
+            }
+            if (this.type === 'prod') {
+                pm2.script = join(__dirname, 'bin/prod.js');
+                pm2.name = 'prod';
+                await pm2.run();
+            }
+        })
     }
 }
