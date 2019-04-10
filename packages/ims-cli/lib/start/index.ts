8a790cc1f834fed682ae81a2bf508d8cebb178ee
imeepos <1037483576@qq.com>
2019-04-10 16:07:29 +0800

    ims-cli:build

--- packages/ims-cli/lib/start/index.ts
+++ packages/ims-cli/lib/start/index.ts
@@ -4,6 +4,14 @@ import { join } from 'path';
 import { exec } from 'shelljs'
 import { StartOptions } from 'pm2'
 import fs from 'fs-extra'
+
+async function execSync(command: string) {
+    return new Promise((resolve, reject) => {
+        exec(command, { cwd: process.cwd() }, () => {
+            resolve();
+        })
+    })
+}
 @Command({
     name: 'start'
 })
@@ -15,44 +23,44 @@ export class ImsStart extends ImsCommand {
 
     async run() {
         const root = process.cwd();
-        exec(`pm2 kill`, { cwd: root }, () => {
-            const devApps: StartOptions[] = [];
-            fs.ensureDirSync(join(root, 'config/pm2'))
-            fs.ensureDirSync(join(root, 'data/logs'))
-            devApps.push({
-                name: 'template_dev',
-                script: join(__dirname, 'bin/template_dev.js'),
-                output: join(root, 'data/logs/template_dev.log'),
-                error: join(root, 'data/logs/template_dev-error.log')
-            })
-            devApps.push({
-                name: 'dev',
-                script: join(__dirname, 'bin/dev.js'),
-                output: join(root, 'data/logs/dev.log'),
-                error: join(root, 'data/logs/dev-error.log')
-            });
-            fs.writeFileSync(join(root, 'config/pm2/dev.json'), JSON.stringify(devApps, null, 2));
-
-            const prodApps: StartOptions[] = [];
-
-            prodApps.push({
-                name: 'template_prod',
-                script: join(__dirname, 'bin/template_prod.js'),
-                output: join(root, 'data/logs/template_prod.log'),
-                error: join(root, 'data/logs/template_prod-error.log')
-            })
-            prodApps.push({
-                name: 'prod',
-                script: join(__dirname, 'bin/prod.js'),
-                output: join(root, 'data/logs/prod.log'),
-                error: join(root, 'data/logs/prod-error.log')
-            });
-            fs.writeFileSync(join(root, 'config/pm2/prod.json'), JSON.stringify(prodApps, null, 2));
-            if (this.dev) {
-                exec(`pm2 start ${join(root, 'config/pm2/dev.json')}`)
-            } else {
-                exec(`pm2 start ${join(root, 'config/pm2/prod.json')}`)
-            }
+        await execSync(`pm2 kill`)
+        const devApps: StartOptions[] = [];
+        fs.ensureDirSync(join(root, 'config/pm2'))
+        fs.ensureDirSync(join(root, 'data/logs'))
+        devApps.push({
+            name: 'template_dev',
+            script: join(__dirname, 'bin/template_dev.js'),
+            output: join(root, 'data/logs/template_dev.log'),
+            error: join(root, 'data/logs/template_dev-error.log')
         })
+        devApps.push({
+            name: 'dev',
+            script: join(__dirname, 'bin/dev.js'),
+            output: join(root, 'data/logs/dev.log'),
+            error: join(root, 'data/logs/dev-error.log')
+        });
+        fs.writeFileSync(join(root, 'config/pm2/dev.json'), JSON.stringify(devApps, null, 2));
+
+        const prodApps: StartOptions[] = [];
+
+        prodApps.push({
+            name: 'template_prod',
+            script: join(__dirname, 'bin/template_prod.js'),
+            output: join(root, 'data/logs/template_prod.log'),
+            error: join(root, 'data/logs/template_prod-error.log')
+        })
+        prodApps.push({
+            name: 'prod',
+            script: join(__dirname, 'bin/prod.js'),
+            output: join(root, 'data/logs/prod.log'),
+            error: join(root, 'data/logs/prod-error.log')
+        });
+        fs.writeFileSync(join(root, 'config/pm2/prod.json'), JSON.stringify(prodApps, null, 2));
+        if (this.dev) {
+            await execSync(`pm2 start ${join(root, 'config/pm2/dev.json')}`)
+        } else {
+            await execSync(`pm2 start ${join(root, 'config/pm2/prod.json')}`)
+        }
+        await execSync(`pm2 list`);
     }
 }
