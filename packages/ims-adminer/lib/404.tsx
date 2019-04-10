cd4c8e130546b85bee13e67107a5f910a3341fb7
imeepos <1037483576@qq.com>
2019-04-10 14:44:37 +0800

    ims-adminer:build

--- packages/ims-adminer/lib/404.tsx
+++ packages/ims-adminer/lib/404.tsx
@@ -1,11 +1,9 @@
 import Exception from 'ant-design-pro/lib/Exception';
-import { Button as AntButton } from 'antd'
 import React from 'react';
-const Button: any = AntButton;
 export function Exception404() {
     return <Exception
         type="404"
-        desc={<div><Button type="dashed">抱歉，你访问的页面不存在</Button></div>}
+        desc={<div>抱歉，你访问的页面不存在</div>}
         backText={"返回"}
     />
 }
