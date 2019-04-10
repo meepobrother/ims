77a5c7aa48b7c25955f1f8545776757462a6c182
imeepos <1037483576@qq.com>
2019-04-10 14:29:04 +0800

    ims-webpack:build

--- packages/ims-webpack/lib/webpack.ts
+++ packages/ims-webpack/lib/webpack.ts
@@ -109,6 +109,25 @@ export abstract class ImsWebpack {
                         }
                     }]
                 }, {
+                    test: /\.jsx?$/,
+                    enforce: 'pre',
+                    use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            babelrc: false,
+                            configFile: false,
+                            presets: ['@babel/preset-env', '@babel/preset-react'],
+                            plugins: [
+                                ['@babel/plugin-syntax-dynamic-import'],
+                                [AntdPlugin, {
+                                    "libraryName": "antd",
+                                    "libraryDirectory": "es",
+                                    "style": true // `style: true` 会加载 less 文件
+                                }]
+                            ]
+                        }
+                    }]
+                }, {
                     test: /\.css$/,
                     use: [{
                         loader: 'style-loader'
