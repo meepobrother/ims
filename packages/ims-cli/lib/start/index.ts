0b2591ea317c00f4823a92ed76fe78efcfea7572
imeepos <1037483576@qq.com>
2019-04-10 15:50:15 +0800

    ims-cli:build

--- packages/ims-cli/lib/start/index.ts
+++ packages/ims-cli/lib/start/index.ts
@@ -1,7 +1,6 @@
 import { Command, Input } from 'ims-core';
 import { ImsCommand } from '../command';
 import { join } from 'path';
-import { ImsCommandPm2 } from '../pm2'
 import { exec } from 'shelljs'
 import { StartOptions } from 'pm2'
 import fs from 'fs-extra'
@@ -16,28 +15,36 @@ export class ImsStart extends ImsCommand {
 
     async run() {
         const root = process.cwd();
-        const devApps: StartOptions[] = [];
-        fs.ensureDirSync(join(root, 'config/pm2'))
-        devApps.push({
-            name: 'template',
-            script: join(__dirname, 'bin/template_dev.js')
-        })
-        devApps.push({
-            name: 'dev',
-            script: join(__dirname, 'bin/dev.js')
-        });
-        fs.writeFileSync(join(root, 'config/pm2/dev.json'), JSON.stringify(devApps, null, 2));
+        exec(`pm2 kill`, { cwd: root }, () => {
+            const devApps: StartOptions[] = [];
+            fs.ensureDirSync(join(root, 'config/pm2'))
+            devApps.push({
+                name: 'template',
+                script: join(__dirname, 'bin/template_dev.js')
+            })
+            devApps.push({
+                name: 'dev',
+                script: join(__dirname, 'bin/dev.js')
+            });
+            fs.writeFileSync(join(root, 'config/pm2/dev.json'), JSON.stringify(devApps, null, 2));
 
-        const prodApps: StartOptions[] = [];
+            const prodApps: StartOptions[] = [];
 
-        prodApps.push({
-            name: 'template',
-            script: join(__dirname, 'bin/template_prod.js')
+            prodApps.push({
+                name: 'template',
+                script: join(__dirname, 'bin/template_prod.js')
+            })
+            prodApps.push({
+                name: 'dev',
+                script: join(__dirname, 'bin/prod.js')
+            });
+            fs.writeFileSync(join(root, 'config/pm2/prod.json'), JSON.stringify(prodApps, null, 2));
+
+            if (this.type === 'dev') {
+                exec(`pm2 start ${join(root, 'config/pm2/dev.json')}`)
+            } else {
+                exec(`pm2 start ${join(root, 'config/pm2/prod.json')}`)
+            }
         })
-        prodApps.push({
-            name: 'dev',
-            script: join(__dirname, 'bin/prod.js')
-        });
-        fs.writeFileSync(join(root, 'config/pm2/prod.json'), JSON.stringify(prodApps, null, 2));
     }
 }
