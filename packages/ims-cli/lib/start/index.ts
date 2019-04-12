8b7b8ac58f0f118d181e442aa46cf120e0e65f71
imeepos <1037483576@qq.com>
2019-04-12 16:34:26 +0800

    ims-cli:build

--- packages/ims-cli/lib/start/index.ts
+++ packages/ims-cli/lib/start/index.ts
@@ -33,6 +33,12 @@ export class ImsStart extends ImsCommand {
             ],
             restart_delay: 5000
         });
+        devApps.push({
+            name: 'template_dev',
+            script: join(__dirname, 'bin/template_dev.js'),
+            output: join(root, 'data/logs/template_dev.log'),
+            error: join(root, 'data/logs/template_dev-error.log')
+        })
         fs.writeFileSync(join(root, 'config/pm2/dev.json'), JSON.stringify(devApps, null, 2));
         const prodApps: StartOptions[] = [];
         prodApps.push({
@@ -46,7 +52,7 @@ export class ImsStart extends ImsCommand {
         fs.writeFileSync(join(root, 'config/pm2/prod.json'), JSON.stringify(prodApps, null, 2));
         if (this.dev) {
             await execSync(`pm2 start ${join(root, 'config/pm2/dev.json')}`)
-            await execSync(`node ${join(__dirname, 'bin/template_dev')}`)
+            // await execSync(`node ${join(__dirname, 'bin/template_dev')}`)
         } else {
             await execSync(`pm2 start ${join(root, 'config/pm2/prod.json')}`)
             await execSync(`node ${join(__dirname, 'bin/template_prod')}`)
