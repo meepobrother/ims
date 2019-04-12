d011c0354e86800a9723c4b5ea50b343c79b32fc
imeepos <1037483576@qq.com>
2019-04-12 12:10:09 +0800

    ims-cli:build

--- packages/ims-cli/lib/start/index.ts
+++ packages/ims-cli/lib/start/index.ts
@@ -27,26 +27,14 @@ export class ImsStart extends ImsCommand {
             script: join(__dirname, 'bin/template_dev.js'),
             output: join(root, 'data/logs/template_dev.log'),
             error: join(root, 'data/logs/template_dev-error.log'),
-            args: [
-                "-r",
-                "ts-node/register",
-                "-r",
-                "tsconfig-paths/register"
-            ],
-            interpreter: join(root, 'node_modules/.bin/ts-node')
+            interpreter: join(root, 'node_modules/.bin/ts-node -r ts-node/register -r tsconfig-paths/register')
         });
         devApps.push({
             name: 'dev',
             script: join(__dirname, 'bin/dev.js'),
             output: join(root, 'data/logs/dev.log'),
             error: join(root, 'data/logs/dev-error.log'),
-            args: [
-                "-r",
-                "ts-node/register",
-                "-r",
-                "tsconfig-paths/register"
-            ],
-            interpreter: join(root, 'node_modules/.bin/ts-node')
+            interpreter: join(root, 'node_modules/.bin/ts-node -r ts-node/register -r tsconfig-paths/register')
         });
         fs.writeFileSync(join(root, 'config/pm2/dev.json'), JSON.stringify(devApps, null, 2));
         const prodApps: StartOptions[] = [];
