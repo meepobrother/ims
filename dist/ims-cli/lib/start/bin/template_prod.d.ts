33eef987bc3062a9b4a6f5d229604b9ff3eadb33
imeepos <1037483576@qq.com>
2019-04-14 01:55:44 +0800

    ims-cli:build

--- dist/ims-cli/lib/start/bin/template_prod.d.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-export declare class ImsStartApp {
-}
-export declare function bootstrap(dev: boolean): Promise<void>;
--- dist/ims-cli/lib/start/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { ImsCommand } from '../command';
-export declare class ImsStart extends ImsCommand {
-    dev: boolean;
-    run(): Promise<void>;
-}
