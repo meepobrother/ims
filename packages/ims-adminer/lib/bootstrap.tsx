3554de02fffcab64b0ef0f7a55541bbf906cf3f2
imeepos <1037483576@qq.com>
2019-04-14 21:48:42 +0800

    ims-adminer:build

--- packages/ims-adminer/lib/bootstrap.tsx
+++ packages/ims-adminer/lib/bootstrap.tsx
@@ -29,44 +29,43 @@ export function createStore(routes: IRouter[]) {
     });
     return ObjectStore;
 }
-export function bootstrap(routes: IRouter[]) {
-    return ImsUtil.onInit(routes).then(() => {
-        const store = createStore(routes);
-        const userRole = store.login.role || 'default'
-        const AuthorizedRoute = Authorized(userRole).AuthorizedRoute;
-        const routerProps: any = (router) => ({
-            authority: role => {
-                if (!router.roles) return true;
-                if (router.roles.length === 0) return true;
-                if (role === 'admin') return true;
-                return router.roles.indexOf(role) > -1;
-            },
-            redirectPath: '/403',
-            key: router.path,
-            path: router.path,
-            exact: !!router.exact,
-            render: () => {
-                if (router.component) {
-                    return <React.Suspense fallback={<Loading />} >
-                        <router.component route={router} />
-                    </React.Suspense>
-                } else {
-                    return <ImsRoutes route={router} />
-                }
+export async function bootstrap(routes: IRouter[]) {
+    await ImsUtil.onInit(routes);
+    const store = createStore(routes);
+    const userRole = store.login.role || 'default'
+    const AuthorizedRoute = Authorized(userRole).AuthorizedRoute;
+    const routerProps: any = (router) => ({
+        authority: role => {
+            if (!router.roles) return true;
+            if (router.roles.length === 0) return true;
+            if (role === 'admin') return true;
+            return router.roles.indexOf(role) > -1;
+        },
+        redirectPath: '/403',
+        key: router.path,
+        path: router.path,
+        exact: !!router.exact,
+        render: () => {
+            if (router.component) {
+                return <React.Suspense fallback={<Loading />} >
+                    <router.component route={router} />
+                </React.Suspense>
+            } else {
+                return <ImsRoutes route={router} />
             }
-        });
-        render(
-            <Provider {...store}>
-                <Router>
-                    {routes.map((route, key) => {
-                        const props = routerProps(route);
-                        console.log(props);
-                        return <AuthorizedRoute {...props} />
-                    })}
-                </Router>
-            </Provider >
-            ,
-            document.getElementById('root')
-        )
-    })
+        }
+    });
+    render(
+        <Provider {...store}>
+            <Router>
+                {routes.map((route, key) => {
+                    const props = routerProps(route);
+                    console.log(props);
+                    return <AuthorizedRoute {...props} />
+                })}
+            </Router>
+        </Provider >
+        ,
+        document.getElementById('root')
+    )
 }
