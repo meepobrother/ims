dad194b3c8aaddc9a0fc053965cd61a38b31caa9
imeepos <1037483576@qq.com>
2019-04-15 21:20:27 +0800

    ims-webpack-admin:build

--- packages/ims-webpack-admin/lib/index.ts
+++ packages/ims-webpack-admin/lib/index.ts
@@ -53,9 +53,6 @@ export class ImsWebpackAdmin extends ImsWebpack {
         this.config.plugin('antdDll').use(DllReferencePlugin, [{
             manifest: require(join(root, 'template/library/antd.manifest.json'))
         }]);
-        this.config.plugin('antDesignIconsDll').use(DllReferencePlugin, [{
-            manifest: require(join(root, 'template/library/antDesignIcons.manifest.json'))
-        }]);
         this.config.plugin('bizchartsDll').use(DllReferencePlugin, [{
             manifest: require(join(root, 'template/library/bizcharts.manifest.json'))
         }]);
