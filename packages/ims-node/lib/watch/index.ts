e39f9b56f98e3b755ce4fefca0d4ad4afad88e6f
imeepos <1037483576@qq.com>
2019-04-15 14:45:56 +0800

    ims-node:build

--- packages/ims-node/lib/watch/index.ts
+++ packages/ims-node/lib/watch/index.ts
@@ -4,22 +4,13 @@ import { visitor } from "ims-common";
 import { watch } from 'chokidar';
 import { AddonMetadataKey, AddonAst } from "ims-core";
 import { ImsApplication } from "../transform/application";
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-const change = new Subject();
-change.pipe(
-    debounceTime(50)
-).subscribe((sourceRoot: string) => {
-    if (ImsApplication.application) {
-        const type = require(sourceRoot).default;
-        console.log(`change ${sourceRoot} ${type.target}`)
-        ImsApplication.application.reInstall(type)
-    }
-});
 export function watchAddon(type: Type<any>) {
     const context = visitor.visitType(type);
     const addonAst = context.getClass(AddonMetadataKey) as AddonAst;
     watch(`${addonAst.sourceRoot}`).on('all', (eventName, path) => {
-        change.next(addonAst.sourceRoot)
+        if (ImsApplication.application) {
+            const type = require(addonAst.sourceRoot).default;
+            ImsApplication.application.reInstall(type);
+        }
     })
 }
