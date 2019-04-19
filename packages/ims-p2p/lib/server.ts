6ebcf22f246d9baa16ec8523eaa186d430c76e54
imeepos <1037483576@qq.com>
2019-04-19 08:23:31 +0800

    ims-p2p:build

--- packages/ims-p2p/lib/server.ts
+++ packages/ims-p2p/lib/server.ts
@@ -3,43 +3,46 @@ import PeerBook from 'peer-book';
 import { createPeerId } from './createPeerId';
 import { Libp2pBundle } from './node';
 import { join } from 'path';
+import fs from 'fs-extra';
 import Libp2p from 'libp2p';
 export async function bootstrap(): Promise<Libp2p> {
-    const config = require(join(process.cwd(), 'config/config.json'))
-    const peerId = await createPeerId('server');
-    const peerInfo = new PeerInfo(peerId);
-    const peerBook = new PeerBook();
-    peerInfo.multiaddrs.add(config.p2p)
-    const node = new Libp2pBundle({
-        peerInfo,
-    }, config.list);
-    node.on('start', () => {
-        discoveredPeers.forEach(putAndDial)
-        discoveredPeers = []
-    });
-    let discoveredPeers = [];
-    const noop = () => { }
-    const putAndDial = (info: PeerInfo) => {
-        info = peerBook.put(info)
-        if (!info.id.isEqual(peerInfo.id)) {
-            if (!info.isConnected()) {
-                node.dial(peerInfo, noop)
+    if (fs.existsSync(join(process.cwd(), 'config/config.json'))) {
+        const config = require(join(process.cwd(), 'config/config.json'))
+        const peerId = await createPeerId('server');
+        const peerInfo = new PeerInfo(peerId);
+        const peerBook = new PeerBook();
+        peerInfo.multiaddrs.add(config.p2p)
+        const node = new Libp2pBundle({
+            peerInfo,
+        }, config.list);
+        node.on('start', () => {
+            discoveredPeers.forEach(putAndDial)
+            discoveredPeers = []
+        });
+        let discoveredPeers = [];
+        const noop = () => { }
+        const putAndDial = (info: PeerInfo) => {
+            info = peerBook.put(info)
+            if (!info.id.isEqual(peerInfo.id)) {
+                if (!info.isConnected()) {
+                    node.dial(peerInfo, noop)
+                }
             }
         }
-    }
-    node.on('peer:discovery', (info) => {
-        if (node.isStarted()) {
-            putAndDial(info)
-        } else {
-            discoveredPeers.push(info)
-        }
-    });
-    node.on('peer:connect', peerInfo => {
-        peerBook.put(peerInfo);
-    });
-    return new Promise((resolve, reject) => {
-        node.start(() => {
-            resolve(node)
+        node.on('peer:discovery', (info) => {
+            if (node.isStarted()) {
+                putAndDial(info)
+            } else {
+                discoveredPeers.push(info)
+            }
         });
-    })
+        node.on('peer:connect', peerInfo => {
+            peerBook.put(peerInfo);
+        });
+        return new Promise((resolve, reject) => {
+            node.start(() => {
+                resolve(node)
+            });
+        })
+    }
 }
